ef9cf91826d9b57c8f306e72f187be2f0aba2b9c
JohnRachid <jrachid@iastate.edu>
2018-04-16 19:13:18 -0500

    Changed varibles to prepare for a second training session. This sessions will be for 25,000 steps.

--- run_this.py
+++ run_this.py
@@ -6,12 +6,15 @@ import cv2
 import time
 
 
+# modified from the tutorial found at
+# https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/tree/master/contents/5_Deep_Q_Network
+
 def run_pocket_aces():
         step = 0
         time.sleep(2) #allows user to click off into DD Poker 3
 
         RL.load("save/build3")
-        for episode in range(100):
+        for episode in range(25000):
 
             # initial observation
             observation = env.reset()
@@ -36,17 +39,29 @@ def run_pocket_aces():
                 observation = observation_
 
                 #saving, MAKE SURE SAVE DIRECTORY IS DELETED
-                # if episode == 3000:
-                #     RL.save(observation, action, reward, observation_, "save/save1")
-                #     RL.build("save/build1")
-                #
-                # if episode == 6000:
-                #      RL.save(observation, action, reward, observation_,"save/save2")
-                #      RL.build("save/build2")
-                #
-                # if episode == 9900:
-                #     RL.save(observation, action, reward, observation_, "save/save3")
-                #     RL.build("save/build3")
+                if episode == 3000:
+                    RL.save(observation, action, reward, observation_, "save/save1")
+                    RL.build("save/build1")
+
+                if episode == 6000:
+                     RL.save(observation, action, reward, observation_,"save/save2")
+                     RL.build("save/build2")
+
+                if episode == 10000:
+                    RL.save(observation, action, reward, observation_, "save/save3")
+                    RL.build("save/build3")
+
+                if episode == 15000:
+                    RL.save(observation, action, reward, observation_, "save/save4")
+                    RL.build("save/build4")
+
+                if episode == 20000:
+                    RL.save(observation, action, reward, observation_, "save/save5")
+                    RL.build("save/build5")
+
+                if episode == 24800:
+                    RL.save(observation, action, reward, observation_, "save/save6")
+                    RL.build("save/build6")
 
                 # break while loop when end of this episode
                 if done:
@@ -68,8 +83,8 @@ if __name__ == "__main__":
                       learning_rate=0.01,
                       reward_decay=0.9,
                       e_greedy=0.9,
-                      replace_target_iter=10000,
-                      memory_size=100000,
+                      replace_target_iter=25000,
+                      memory_size=200000,
                       # output_graph=True
                       )
     #env.after(100, run_pocket_aces)
